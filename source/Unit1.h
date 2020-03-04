02a095c8c135a800bbcda08b55e10b4aa597d787
Yaroslav <61201443+yaros1avsv@users.noreply.github.com>
2020-03-04 15:19:19 +0500

    get start
    Пустая форма и пара кнопок

---
+++ source/Unit1.h
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include "VirtualTrees.hpp"
+
+// переменная структуры VST
+struct VSTStruct
+{
+	int id;
+	UnicodeString url;
+	UnicodeString data;
+};
+
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *OpenButton;
+	TButton *RemoveButton;
+	TButton *ClearAllButton;
+	TVirtualStringTree *VirtualStringTree1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
